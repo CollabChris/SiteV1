ec79383b22a5684f03e8f0a369d4ea54388b6f0f
Chris Dalley <chris@collaborate.co.uk>
2013-01-28 14:10:56 +0000

    Updated routes and deleted index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SiteV1::Application.routes.draw do
 
-	root :to => "static_pages#home"
+	root :to => 'static_pages#home'
 	
-	match "/contact" => "static_pages#contact"
-	match "/about" => "static_pages#about"
+	match '/contact' => 'static_pages#contact'
+	
+	match '/about' => 'static_pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
